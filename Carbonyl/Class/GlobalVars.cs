6f90a7b8a126a988eb0e3aaf716ba507f8cf4774
Jack Lei <3486688394@qq.com>
2023-12-10 04:04:00 +0800

    (改) 为 Carbonyl 修改多语言国际化

---
+++ Carbonyl/Class/GlobalVars.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace Carbonyl.Class;
+
+public static class GlobalVars
+{
+    public static class Internationalization
+    {
+        public static CultureInfo SystemDefaultLanguage { get; set; } = null!;
+    }
+}
--- Carbonyl/Program.cs
+++ Carbonyl/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Carbonyl;
+﻿using System.Globalization;
+using Carbonyl.Class;
+using Carbonyl.Resources.Languages;
+
+namespace Carbonyl;
 
 public static class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /* [ 国际化 - 初始化 ] */
+        GlobalVars.Internationalization.SystemDefaultLanguage = Thread.CurrentThread.CurrentUICulture;
     }
 }
