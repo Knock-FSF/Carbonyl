d0bc32bc3ff9b99c70becfc9f1cb2bb6dd67f664
Jack Lei <3486688394@qq.com>
2023-12-11 12:34:20 +0800

    (改) 删除冗余代码

--- Carbonyl/Class/CommandHandler/CommandManager.cs
+++ Carbonyl/Class/CommandHandler/CommandManager.cs
@@ -37,49 +37,4 @@ public class CommandManager
     {
         return await _commandRunner.ExecuteAsync(Commands, args).ConfigureAwait(false);
     }
-
-
-    /*
-    private readonly string _identifier;
-    private readonly List<CommandManager> _subcommands;
-    public CommandManager(string? identifier, List<CommandManager>? subcommands) {
-        _identifier = identifier ?? throw new NullReferenceException("Command identifier cannot be null");
-        _subcommands = subcommands ?? new List<CommandManager>(0);
-    }
-
-    public CommandManager(string? identifier) {
-        _identifier = identifier ?? throw new NullReferenceException("Command identifier cannot be null");
-        _subcommands = new List<CommandManager>(0);
-    }
-
-    public delegate Task<int> ExecDelegate();
-    public delegate Task<int> ExecArgsDelegate(string[] args);
-
-    public ExecDelegate? ExecFunction { get; init; }
-    public ExecArgsDelegate? ExecArgsFunction { get; init; }
-
-    public async Task<int> Exec()
-    {
-        return await(ExecFunction?.Invoke() ?? Task.FromResult(0));
-    }
-
-    public async Task<int> Exec(string[] args) {
-        return await(ExecArgsFunction?.Invoke(args) ?? Task.FromResult(0));
-    }
-
-    protected void AddSubcommand(CommandManager? newSubcommand) {
-        if (newSubcommand is null)
-            throw new NullReferenceException("New command cannot be null");
-
-        _subcommands.Add(newSubcommand);
-    }
-
-    public string GetIdentifier() {
-        return _identifier;
-    }
-
-    public List<CommandManager> GetSubCommands() {
-        return _subcommands;
-    }
-    */
 }
